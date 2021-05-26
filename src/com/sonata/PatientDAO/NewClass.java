2b0ce2d68967a632e06feb2f4345d500c8b587ae
Bhagwat Chiddarwar <bhagwat.chiddarwar@sonata-software.com>
2021-05-26 10:51:50 +0530

    my commit in suhas repo

---
+++ src/com/sonata/PatientDAO/NewClass.java
@@ -0,0 +1,10 @@
+package com.sonata.PatientDAO;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bhagwat commit");
+	}
+
+}
